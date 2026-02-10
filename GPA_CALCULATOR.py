c557d35e52c78d2d6894aa72378b985b95fe2a5b
Dsalas8 <ssdamian1218@gmail.com>
2026-02-10 20:49:04 +0000

    edits

--- GPA_CALCULATOR.py
+++ GPA_CALCULATOR.py
@@ -1,1 +1,8 @@
 #Hello my name is damian.   
+
+print("Hello and welcome to GPATRACKER! The place where managing and viewing grades is easier than ever. Whenever you're ready, lets get started!")
+print("Select the cloass you'd like to view")
+
+class = input("Math, English, Science, Social Studies, Spanish, PE")
+if class == "Math":
+    print("HI")
