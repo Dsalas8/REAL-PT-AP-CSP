26c9f288c1fda0aab99354a0a32e5e9c3685bb45
Dsalas8 <ssdamian1218@gmail.com>
2026-02-24 21:10:19 +0000

    edits

--- GPA_CALCULATOR.py
+++ GPA_CALCULATOR.py
@@ -95,11 +95,11 @@ def function_3():
     answer = input("Would you like to review your grades once more? ")  #Give user last chance to view grades.
     if answer == "yes":
         print("---------------------------")
-        function_2()  
-    import sys                          #If yes, directs them back to function where the block of code is repeated.
+        function_2()  #If yes, directs them back to function where the block of code is repeated.
+    import sys                          
     if answer == "no":
         print("Have a good day!")
-        sys.exit()                     #Ends program.
+        sys.exit()                     #Ends program from any function.
      
          
     
